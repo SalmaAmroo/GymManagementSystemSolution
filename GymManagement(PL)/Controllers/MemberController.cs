7ce92f1f7290058a358f7ef469b5a32e949ded1e
Admin <Admin@SALMAAMRO>
2025-12-26 00:41:08 +0200

    s5

--- GymManagement(PL)/Controllers/MemberController.cs
+++ GymManagement(PL)/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 ﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.MemberViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
@@ -18,16 +19,63 @@ namespace GymManagementPL.Controllers
         }
         public ActionResult MemberDetails(int id)
         {
-            if(id==0) 
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id Number Of Member Can Nor Be 0 Or Negative Number";
                 return RedirectToAction(nameof(Index));
-
+            }
             var Member = _memberservice.GetMemberDetails(id);
 
-            if(Member is null)
+            if (Member is null)
+            {
+                TempData["ErrorMessage"] = "Member Not Found";
                 return RedirectToAction(nameof(Index));
-
+            }
             return View(Member);
         }
+        public ActionResult HealthRecordDetails(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id Number Of Member Can Nor Be 0 Or Negative Number";
+                return RedirectToAction(nameof(Index));
+            }
+            var HealthRecord = _memberservice.GetHealthRecord(id);
+            if (HealthRecord is null)
+            {
+                TempData["ErrorMessage"] = "HealthRecord Not Found";
+                return RedirectToAction(nameof(Index));
+            }
+                return View(HealthRecord);
+
+        }
+
+        #region Create Member
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateMember(CreateMemberViewModel CreatedMember)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("DataInValid", "Check Data And Missing Fields");
+                return View(nameof(Create), CreatedMember);
+            }
+           bool Result= _memberservice.CreateMember(CreatedMember);
+            if (Result)
+            {
+                TempData["SuccessMessage"] = "Member Created Successfully";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Failed To Create Member , Check Phone And Email";
+            }
+
+                return RedirectToAction(nameof(Index));
+        }
+        #endregion
     }
 }
     
